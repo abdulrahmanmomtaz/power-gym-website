6b927441d9217c7a8f39394f3ed0871f8012a9b6
Abdulrahman momtaz <abdalrahaman.mohamed685@sh-eng.menofia.edu.eg>
2025-08-13 11:28:59 +0300

    signup

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -28,7 +28,7 @@ return (
             <li><a href="#testimonials" onClick={toggleMenu}>Testimonials</a></li>
             <li><a href="#gallery" onClick={toggleMenu}>Facilities</a></li>
             <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
-            <li className="mobile-join"><a href="/src/component/AuthPage.jsx" className="btn-join">Join Us</a></li>
+            <Link to="/src/component/AuthPage.jsx" className="btn-join">Join us</Link>
         </ul>
 
         <div className="account desktop-only">
